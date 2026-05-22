b4712b30f4d71f64933195df948f28f747c6e67c
Your Name <you@example.com>
2026-05-22 06:21:58 +0100

    lesson 06 enhancements

--- 06-create-bucket/monorepo/cdk/src/bin/cloudfront.ts
+++ 06-create-bucket/monorepo/cdk/src/bin/cloudfront.ts
@@ -1,5 +1,5 @@
 import * as cdk from "aws-cdk-lib";
-import { CloudfrontStack } from "../lib/cloudfrontStack.js";
+import { CloudfrontStack } from "../lib/cloudfrontStack";
 
 const app = new cdk.App();
 new CloudfrontStack(app, "cloudfront-website");

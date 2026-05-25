68c888d9b9263a1af5bf5ca3f388dc4a5b24c449
Your Name <you@example.com>
2026-05-25 12:08:22 +0100

    updated lesson 04

--- 04-add-vite-website/monorepo/cdk/src/lib/cloudfrontStack.ts
+++ 04-add-vite-website/monorepo/cdk/src/lib/cloudfrontStack.ts
@@ -4,7 +4,6 @@ import {
   ViewerProtocolPolicy,
 } from "aws-cdk-lib/aws-cloudfront";
 import { S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
-import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
 import type { Construct } from "constructs";
 
@@ -25,25 +24,6 @@ export class CloudfrontStack extends Stack {
         origin: S3BucketOrigin.withOriginAccessControl(websiteBucket),
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
-      errorResponses: [
-        {
-          httpStatus: 403,
-          responseHttpStatus: 200,
-          responsePagePath: "/index.html",
-        },
-        {
-          httpStatus: 404,
-          responseHttpStatus: 200,
-          responsePagePath: "/index.html",
-        },
-      ],
-    });
-
-    new BucketDeployment(this, "PlaceholderWebsiteDeployment", {
-      sources: [Source.asset("./src/website")],
-      destinationBucket: websiteBucket,
-      distribution,
-      distributionPaths: ["/*"],
     });
 
     new CfnOutput(this, "CloudfrontDistributionDomainName", {

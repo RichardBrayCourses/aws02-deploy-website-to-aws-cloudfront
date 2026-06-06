90fd445aa4920a3a281bd042e5c1917aa18ec665
Your Name <you@example.com>
2026-06-06 09:25:12 +0100

    updated lesson 8

--- 08-advanced-cdk-cleanup/monorepo/scripts/bootstrap-down.sh
+++ 08-advanced-cdk-cleanup/monorepo/scripts/bootstrap-down.sh
@@ -4,33 +4,7 @@
 
 aws cloudformation delete-stack --stack-name "CDKToolkit" --region "eu-west-2"
 
-# Delete the CDK bootstrap stack from us-east-1
-# This region is commonly used for CloudFront SSL certificates
 
-aws cloudformation delete-stack --stack-name "CDKToolkit" --region "us-east-1"
+# The CDK bootstrap process may leave behind versioned S3 buckets containing old deployment assets
 
-# -----------------------------------------------------------------------------
-# ADVANCED CLEANUP (OPTIONAL)
-# -----------------------------------------------------------------------------
-#
-# The CDK bootstrap process may leave behind:
-#
-# - Versioned S3 buckets containing old deployment assets
-# - ECR repositories containing container images
-#
-# These resources can incur small ongoing storage costs.
-#
-# The following scripts permanently delete those resources and should only
-# be used if you fully understand what they do.
-#
-# DANGEROUS:
-# - Permanently deletes versioned S3 objects
-# - Permanently deletes ECR container images
-# - Cannot be undone
-#
-# Uncomment to run:
-#
 ./bootstrap-delete-buckets.sh
-./bootstrap-delete-containers.sh
-#
-# -----------------------------------------------------------------------------
